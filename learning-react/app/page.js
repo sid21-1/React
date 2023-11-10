ea7e9fa4868ff32f0d3d5fd424841f7ef4bb2d24
Siddhanth Gupta <guptasiddhanth2@gmail.com>
2023-11-10 12:47:05 +0530

    react usestate and variables to declare in react

--- learning-react/app/page.js
+++ learning-react/app/page.js
@@ -1,15 +1,18 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 
 const page = () => {
-  const groom = "salmon";
-  const bride = "kat";
-  let marks = 80;
-  const changeMarks = () => {
-    console.log(marks);
-    marks = 33;
-    console.log(marks);
-  };
+  // const groom = "salmon";
+  // const bride = "kat";
+  // let marks = 80;
+  // const changeMarks = () => {
+  //   console.log(marks);
+  //   marks = 33;
+  //   console.log(marks);
+  // };
+
+  //react usestate and variables to declare in react
+  const [marks, setMarks] = useState(60);
 
   return (
     // fragments tags <></>
@@ -17,7 +20,7 @@ const page = () => {
       <h1 className=" font-bold text-4xl ">hello hi {marks}</h1>
       <button
         onClick={() => {
-          changeMarks();
+          setMarks(33);
         }}
         className=" bg-orange-600 px-5 py-2 rounded mt-5 mx-3 text-white font-bold"
       >
