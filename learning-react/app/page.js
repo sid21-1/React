24410bbf44a35a30aae17806b649dd1068d20d0c
Siddhanth Gupta <guptasiddhanth2@gmail.com>
2023-11-10 12:11:41 +0530

    writing jsx code and changed css styling

--- learning-react/app/page.js
+++ learning-react/app/page.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
 const page = () => {
+  const groom = "salmon";
+  const bride = "kat";
   return (
-    <div>Hello world</div>
+    <div>{groom} weds {bride}</div>
   )
 }
 
