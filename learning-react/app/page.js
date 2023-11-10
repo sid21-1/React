d1018ed6bfde329d740deaa30287c7697cf1ac0a
Siddhanth Gupta <guptasiddhanth2@gmail.com>
2023-11-10 12:40:24 +0530

    updating values using react

--- learning-react/app/page.js
+++ learning-react/app/page.js
@@ -1,11 +1,30 @@
-import React from 'react'
+"use client";
+import React from "react";
 
 const page = () => {
   const groom = "salmon";
   const bride = "kat";
-  return (
-    <div>{groom} weds {bride}</div>
-  )
-}
+  let marks = 80;
+  const changeMarks = () => {
+    console.log(marks);
+    marks = 33;
+    console.log(marks);
+  };
 
-export default page
+  return (
+    // fragments tags <></>
+    <>
+      <h1 className=" font-bold text-4xl ">hello hi {marks}</h1>
+      <button
+        onClick={() => {
+          changeMarks();
+        }}
+        className=" bg-orange-600 px-5 py-2 rounded mt-5 mx-3 text-white font-bold"
+      >
+        Update
+      </button>
+    </>
+  );
+};
+
+export default page;
