e50e7ef858c26bb8ff6b989f92c145b38a94d2fd
Siddhanth Gupta <guptasiddhanth2@gmail.com>
2023-11-17 11:35:50 +0530

    created the html document of the todolist

---
+++ todolist/app/page.js
@@ -0,0 +1,31 @@
+"use client"
+import React, { useState } from "react";
+
+const page = () => {
+  const [title, settitle] = useState("");
+  const [disc, setdisc] = useState("");
+  return (
+    <>
+      <h1 className="bg-black text-white p-5 text-5xl font-bold text-center">
+        My Todo List
+      </h1>
+      <form action="">
+        <input
+          type="text"
+          className="text-2xl border-zinc-800 border-2  m-5 px-4 py-2"
+          placeholder="Enter Task here"
+        />
+        <input
+          type="text"
+          className="text-2xl border-zinc-800 border-2  m-5 px-4 py-2"
+          placeholder="Enter Description here"
+        />
+        <button className=" bg-black text-white px-4 py-3  text-2xl fond-bold rounded  m-5">
+          Add Task
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default page;
