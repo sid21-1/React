1908fbdf7e9973c890d921e629496c1949b365c8
Siddhanth Gupta <guptasiddhanth2@gmail.com>
2024-02-03 12:46:23 +0530

    created store action and reducer in redux

--- redux toolkit/src/app/store.js
+++ redux toolkit/src/app/store.js
@@ -1,7 +1,8 @@
 //create the store
 
 import { configureStore } from "@reduxjs/toolkit";
+import todoReducer from "../feature/todo/todoSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: todoReducer,
 });
