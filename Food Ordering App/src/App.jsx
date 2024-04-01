5ad19ba65dbc73bb2a251c8f2d10295aef646237
Siddhanth Gupta <guptasiddhanth2@gmail.com>
2024-04-01 09:28:49 +0530

    learnt the basics of routing using react router dom

--- Food Ordering App/src/App.jsx
+++ Food Ordering App/src/App.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 
 function App() {
   return (
@@ -11,5 +15,28 @@ function App() {
     </>
   );
 }
+const appRoute = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement:<Error/>
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
 
-export default App;
+function AppRouter() {
+  return (
+    <RouterProvider router={appRoute}>
+      <App />
+    </RouterProvider>
+  );
+}
+
+export default AppRouter;
---
+++ Food Ordering App/src/components/Error.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useRouteError } from "react-router-dom";
+
+const Error = () => {
+  const err = useRouteError();
+  console.log(err);
+  return (
+    <div>
+      <h2>Page not found </h2>
+      <h3>
+        {err.status} {err.statusText}
+      </h3>
+    </div>
+  );
+};
+
+export default Error;
