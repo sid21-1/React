4da800ce293758057000543b80ee7c86c6e92cc5
Siddhanth Gupta <guptasiddhanth2@gmail.com>
2024-04-03 12:53:12 +0530

    class based components state variable and Api calls

--- Food Ordering App/src/components/User.jsx
+++ Food Ordering App/src/components/User.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
+
 const User = ({ name }) => {
+  const [count, setCount] = useState(0);
   return (
     <div className="user-card">
       <h1>{name} Gupta</h1>
+      <h2>Count = {count}</h2>
       <h3>Location : Darjeeling </h3>
       <h4>Contact: sid</h4>
     </div>
--- Food Ordering App/src/components/UserClass.jsx
+++ Food Ordering App/src/components/UserClass.jsx
@@ -3,15 +3,45 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
-    console.log(props);
+    // console.log(props);
+    this.state = {
+      userInfo: {
+        name: "Dunny",
+        location: "Default",
+        avatar_url: "no  Photo",
+      },
+    };
   }
+
+  async componentDidMount() {
+    const data = await fetch("https://api.github.com/users/sid21-1");
+
+    const json = await data.json();
+
+    this.setState({
+      userInfo: json,
+    });
+    // console.log(json);
+  }
+
   render() {
-    const { name } = this.props;
+    const { name, location, avatar_url } = this.state.userInfo;
     return (
       <div className="user-card">
-        <h1>{name} Gupta</h1>
-        <h3>Location : Darjeeling </h3>
-        <h4>Contact: sid</h4>
+        <h2>{name}</h2>
+        {/* <button
+          onClick={() => {
+            this.setState({
+              count: this.state.count + 1,
+            });
+          }}
+        >
+          Count Increase
+        </button> */}
+        <h1>{location}</h1>
+        {/* <h3>Location : Darjeeling </h3> */}
+        {/* <h4>Contact: sid</h4> */}
+        <img src={avatar_url} alt="" />
       </div>
     );
   }
